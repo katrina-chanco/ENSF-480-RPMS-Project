582bc8886cf903791ccafc29d878c4895d81ed1c
evan-krul <evan@krul.ca>
2019-11-13 16:38:24 -0700

    Added JPA and example Name

---
+++ src/main/java/com/RPMS/model/Name.java
@@ -0,0 +1,53 @@
+package com.RPMS.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Name {
+    private int id;
+    private String fName;
+    private String mName;
+    private String lName;
+
+    public Name(String fName, String mName, String lName) {
+        this.fName = fName;
+        this.mName = mName;
+        this.lName = lName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+}
--- target/frontend/generated-flow-imports.js
+++ target/frontend/generated-flow-imports.js
@@ -10,5 +10,3 @@ import '@vaadin/vaadin-lumo-styles/style.js';
 import '@vaadin/vaadin-lumo-styles/icons.js';
 import '@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-button/theme/lumo/vaadin-button.js';
-import '@vaadin/flow-frontend/flow-component-renderer.js';
-import '@vaadin/vaadin-notification/theme/lumo/vaadin-notification.js';
